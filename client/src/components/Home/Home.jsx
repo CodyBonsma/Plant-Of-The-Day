bcd93c199c097fe1f7eea707aeaba5c33da751cd
Cody Bonsma <codybonsma@gmail.com>
2021-09-06 18:27:33 -0400

    trying to send data from back to front

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from "react";
 import "./Home.css";
-
-const [plant, setPlant] = useState();
-
-const Plants = () => {
-useEffect(() => {
-    
-})
-}
-
+import axios from "axios";
 
 const Home = () => {
+  const [plant, setPlant] = useState();
+
+  useEffect(() => {
+    axios
+      .get("/api/plants")
+      .then((res) => {
+        console.log("retrieved plants", res);
+      })
+      .catch((err) => {
+        if (err) throw err;
+      });
+  }, []);
+
   return (
     <div>
       <h3>this will be the homepage</h3>
