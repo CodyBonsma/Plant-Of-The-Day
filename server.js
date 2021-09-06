797fc6872e85696e00341f36a53f02f6f99177a4
Cody Bonsma <codybonsma@gmail.com>
2021-09-06 18:44:02 -0400

    changes to packagejson to fix proxy errors

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "public")));
 
 const plants = ["rose", "hydreangia", "bitches", "cookout"];
 
 app.get("/api/plants", (req, res) => {
   res.json(plants);
-//   res.json({success: "true"})
+  //   res.json({success: "true"})
+});
+
+app.post("/api/plants", (req, res) => {
+  res.json(plants);
 });
 
 app.listen(PORT, () => {
